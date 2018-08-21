5ce74b39a6e244fa494e69e2aefda75a619dd6f6
DESKTOP-L01BMQS\OddMary <maryjane_1992@mail.ru>
2018-08-21 21:17:32 +0300

    add webDriver runner

---
+++ Project/Project/UI/Browser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace Project.UI
+{
+    public class Browser
+    {
+        private static Browser _instanse;
+        private static string BrowserName = Config.Default.Browser;
+
+        public readonly IWebDriver WebDriver;
+
+        public static Browser Instance
+        {
+            get
+            {
+                if (_instanse is null)
+                {
+                    Driver browser;
+                    DriverOptions options;
+
+                    switch (BrowserName)
+                    {
+                        case "CHROME":
+                            browser = Driver.CHROME;
+                            options = new ChromeOptions();
+                            break;
+                        //case "FIREFOX":
+                        //    browser = Driver.FIREFOX;
+                        //    options = new FirefoxOptions();
+                        //    break;
+                        default:
+                            browser = Driver.CHROME;
+                            options = new ChromeOptions();
+                            break;
+                    }
+
+                    _instanse = new Browser(WebDriverFactory.GetDriver(browser, options));
+                }
+                return _instanse;
+            }
+        }
+
+        public Browser(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+
+        public void MaximizeBrowserWindow()
+        {
+            Instance.WebDriver.Manage().Window.FullScreen();
+        }
+
+        public void stopBrowser()
+        {
+            try
+            {
+                if (WebDriver != null)
+                {
+                    WebDriver.Quit();
+                }
+            }
+            finally
+            {
+                _instanse = null;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Instance.WebDriver.Navigate().GoToUrl(" https://comaqa.by");
+        }
+    }
+}
---
+++ Project/Project/UI/WebDriverFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace Project.UI
+{
+    public class WebDriverFactory
+    {
+        public readonly IWebDriver WebDriver;
+
+        public static IWebDriver GetDriver(Driver driverType, DriverOptions options)
+        {            
+            var directory = AppDomain.CurrentDomain.BaseDirectory + Config.Default.directory;
+            var timeout = TimeSpan.FromMilliseconds(Config.Default.timeout);
+            switch (driverType)
+            {
+                case Driver.CHROME:
+                    return new ChromeDriver(ChromeDriverService.CreateDefaultService(directory), options as ChromeOptions, timeout);
+                    //case Driver.Firefox:
+                    //    return new FirefoxDriver(FirefoxDriverService.CreateDefaultService(directory), options as FirefoxOptions, timeout);
+            }
+
+            return null;
+        }
+    }
+}
